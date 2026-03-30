c77869c3690a80fcb55dd7fc39decc15128634bf
kimyega <kimyega@naver.com>
2026-03-30 15:19:02 +0900

    NoSQL

---
+++ src/main/java/kopo/poly/persistance/mongodb/impl/MelonMapper.java
@@ -0,0 +1,135 @@
+package kopo.poly.persistance.mongodb.impl;
+
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import kopo.poly.dto.MelonDTO;
+import kopo.poly.persistance.mongodb.AbstractMongoDBComon;
+import kopo.poly.persistance.mongodb.IMelonMapper;
+import kopo.poly.util.CmmUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MelonMapper extends AbstractMongoDBComon implements IMelonMapper {
+
+    private final MongoTemplate mongodb;
+
+    @Override
+    public int insertSong(List<MelonDTO> pList, String colNm) throws Exception {
+
+        log.info("{}.insertSong Start!", this.getClass().getName());
+
+        int res;
+
+        if (super.createCollection(mongodb, colNm, "collectTime")) {
+            log.info("{} 생성되었습니다.", colNm);
+        }
+
+        MongoCollection<Document> col = mongodb.getCollection(colNm);
+
+        for (MelonDTO pDTO : pList) {
+            col.insertOne(new Document(new ObjectMapper().convertValue(pDTO, Map.class)));
+        }
+
+        res = 1;
+
+        log.info("{}.insertSong End!", this.getClass().getName());
+
+        return res;
+    }
+
+    @Override
+    public List<MelonDTO> getSongList(String colNm) throws Exception {
+
+        log.info("{}.getSongList Start!", this.getClass().getName());
+
+
+        List<MelonDTO> rList = new LinkedList<>();
+
+        MongoCollection<Document> col = mongodb.getCollection(colNm);
+
+        Document projection = new Document();
+        projection.append("song", "$song");
+        projection.append("singer", "$singer");
+
+        projection.append("_id", 0);
+
+        FindIterable<Document> rs = col.find(new Document()).projection(projection);
+
+        for (Document doc : rs) {
+            String song = CmmUtil.nvl(doc.getString("song"));
+            String singer = CmmUtil.nvl(doc.getString("singer"));
+
+            log.info("song : {}/ singer : {}", song, singer);
+
+            MelonDTO rDTO = MelonDTO.builder().song(song).singer(singer).build();
+
+            rList.add(rDTO);
+        }
+
+        log.info("{}.getSongList End!", this.getClass().getName());
+
+        return rList;
+    }
+
+    @Override
+    public List<MelonDTO> getSingerSongCnt(String colNm) throws Exception {
+
+        log.info("{}.getSingerSongCnt Start!", this.getClass().getName());
+
+        List<MelonDTO> rList = new LinkedList<>();
+
+        List<? extends Bson> pipeline = Arrays.asList(
+                new Document()
+                        .append("$group", new Document()
+                                .append("_id", new Document()
+                                        .append("singer", "$singer")
+                                )
+                                .append("COUNT(singer)", new Document()
+                                        .append("$sum", 1)
+                                )
+                        ),
+                new Document()
+                        .append("$project", new Document()
+                                .append("singer", "$_id.singer")
+                                .append("singerCnt", "$COUNT(singer)")
+                                .append("_id", 0)
+                        ),
+                new Document()
+                        .append("$sort", new Document()
+                                .append("singerCnt", -1)
+                        )
+        );
+
+        MongoCollection<Document> col = mongodb.getCollection(colNm);
+        AggregateIterable<Document> rs = col.aggregate(pipeline).allowDiskUse(true);
+
+        for (Document doc : rs) {
+            String singer = doc.getString("singer");
+            int singerCnt = doc.getInteger("singerCnt", 0);
+
+            log.info("singer : {}/ singerCnt : {}", singer, singerCnt);
+
+            MelonDTO rDTO = MelonDTO.builder().singer(singer).singerCnt(singerCnt).build();
+
+            rList.add(rDTO);
+        }
+
+        log.info("{}.getSingerSongCnt End!", this.getClass().getName());
+
+        return rList;
+    }
+}
---
+++ src/main/java/kopo/poly/service/IMelonService.java
@@ -0,0 +1,14 @@
+package kopo.poly.service;
+
+import kopo.poly.dto.MelonDTO;
+
+import java.util.List;
+
+public interface IMelonService {
+
+    int collectMelonSong() throws Exception;
+
+    List<MelonDTO> getSongList() throws Exception;
+
+    List<MelonDTO> getSingerSongCnt() throws Exception;
+}
