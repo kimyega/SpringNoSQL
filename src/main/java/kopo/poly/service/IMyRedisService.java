32e0e7ad5046616c2847a5a72476a340b7d7b5db
kimyega <kimyega@naver.com>
2026-04-22 15:52:04 +0900

    NoSQL

---
+++ src/main/java/kopo/poly/service/IMyRedisService.java
@@ -0,0 +1,14 @@
+package kopo.poly.service;
+
+import kopo.poly.dto.RedisDTO;
+
+import java.util.List;
+
+public interface IMyRedisService {
+
+    RedisDTO saveString(RedisDTO pDTO) throws Exception;
+
+    RedisDTO saveStringJSON(RedisDTO pDTO) throws Exception;
+
+    List<String> saveList(List<RedisDTO> pList) throws Exception;
+}
