4862e2276dbeab6023e38d134947a93100905947
kimyega <kimyega@naver.com>
2026-05-11 14:49:46 +0900

    NoSQL

--- src/main/java/kopo/poly/persistance/mongodb/AbstractMongoDBComon.java
+++ src/main/java/kopo/poly/persistance/mongodb/AbstractMongoDBComon.java
@@ -9,6 +9,12 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @RequiredArgsConstructor
 public abstract class AbstractMongoDBComon {
 
+    /**
+     *
+     * @param mongodb   접속된 Mongodb
+     * @param colNm     생성할 컬렉션명
+     * @return          생성 결과
+     */
     protected boolean createCollection(MongoTemplate mongodb, String colNm) {
 
         boolean res;
@@ -23,6 +29,13 @@ public abstract class AbstractMongoDBComon {
         return res;
     }
 
+    /**
+     *
+     * @param mongodb   접속된 Mongodb
+     * @param colNm     생성할 컬렉션명
+     * @param index     생성할 인덱스
+     * @return          생성 결과
+     */
     protected boolean createCollection(MongoTemplate mongodb, String colNm, String[] index ) {
 
         log.info("{}.createCollection Start!", this.getClass().getName());
@@ -46,6 +59,13 @@ public abstract class AbstractMongoDBComon {
         return res;
     }
 
+    /**
+     *
+     * @param mongodb   접속된 MongoDB
+     * @param colNm     생성할 컬렉션명
+     * @param index     생성할 인덱스
+     * @return          생성결과
+     */
     protected boolean createCollection(MongoTemplate mongodb, String colNm, String index) {
 
         String[] indexArr = {index};
@@ -53,6 +73,12 @@ public abstract class AbstractMongoDBComon {
         return createCollection(mongodb, colNm, indexArr);
     }
 
+    /**
+     *
+     * @param mongodb   접속된 MongoDB
+     * @param colNm     생성할 컬렉션명
+     * @return          삭제결과
+     */
     protected boolean dropCollection(MongoTemplate mongodb, String colNm) {
 
         boolean res = false;
